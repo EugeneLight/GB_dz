b78d6907e3a41df68817dadfd27b4f78881cac17
EugeneLight <93005632+EugeneLight@users.noreply.github.com>
2021-11-21 14:43:00 +0500

    Подредактировал второе задание

--- dz_02.py
+++ dz_02.py
@@ -14,28 +14,24 @@ print(cube_list)                        # выводим список
 for n in cube_list:
     num_sum = 0                         # сбрасываем сумму
     while n >= 10:
-        n_1 = n % 10                    # отсекаем последнюю цифру
-        num_sum += n_1                  # добавляем её в сумму цифр
+        num_sum += n % 10               # добавляем в сумму последнюю цифру
         n = n // 10                     # уменьшаем N на цифру
-    if n < 10:                          # если осталась 1 цифра
-        num_sum += n                    # добавляем в сумму остаток
+    num_sum += n                        # добавляем в сумму остаток
     if num_sum % 7 == 0:                # если сумма цифр кратна 7
         result_1 += cube_list[i]        # добавляем число в итоговую сумму
     i += 1
 print(result_1)                         # выводим итоговую сумму
 i = 0
 for i, k in enumerate(cube_list):       # перебираем список
-    cube_list[i] = k + 17               # увеличиваем каждое значение на 17
+    cube_list[i] += 17                  # увеличиваем каждое значение на 17
 print(cube_list)
 i = 0
 for n in cube_list:
     num_sum = 0
     while n >= 10:
-        n_1 = n % 10
-        num_sum += n_1
+        num_sum += n % 10
         n = n // 10
-    if n < 10:
-        num_sum += n
+    num_sum += n
     if num_sum % 7 == 0:
         result_2 += cube_list[i]
     i += 1
