78ce81255186a3e1354511cc00c05c24264276fc
EugeneLight <93005632+EugeneLight@users.noreply.github.com>
2021-12-22 16:32:11 +0500

    поправил задания урока 7

--- Lesson_07/dz_07_03.py
+++ Lesson_07/dz_07_03.py
@@ -1,11 +1,14 @@
 from os import path, getcwd, walk
 from shutil import copytree
 
-full_path = path.join(getcwd(), 'my_project')
 
-for root, dirs, files in walk(full_path):
-    for file in files:
-        if file.rsplit('.')[-1].lower() == 'html'\
-                and not path.exists(path.join(path.join(full_path, 'templates'),
-                                              path.relpath(root, full_path).split('\\')[0])):
-            copytree(root, path.join(path.join(full_path, 'templates'), path.relpath(root, full_path).split('\\')[0]))
+def templates():
+    full_path = path.join(getcwd(), 'my_project')
+    for root, dirs, files in walk(full_path):
+        for file in files:
+            to_path = path.join(path.join(full_path, 'templates'), path.relpath(root, full_path).split('\\')[0])
+            if file.rsplit('.')[-1].lower() == 'html' and not path.exists(to_path):
+                copytree(root, to_path)
+
+
+templates()
